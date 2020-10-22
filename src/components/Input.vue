8f7606ceb26f3dd3bdd49566244496d5303a292a
Nietsab <51116037+Nietsab@users.noreply.github.com>
2020-10-22 09:11:45 +0200

    22/10/2020

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="input">
+    <InputBlock
+      v-bind:value="value"
+      v-bind:placeholder="placeholder"
+      v-on:input="$emit('input', $event.target.value)">
+    </InputBlock>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputBlock',
+  props: {
+    value : String,
+    placeholder: String
+  }
+}
+</script>
